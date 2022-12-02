c90a3998bab4154227f44475d2189f76e5911ae1
Nicolas Rodriguez <nicolasarielrodriguez@gmail.com>
2022-12-02 18:43:54 -0300

    file env

--- src/firebase.js
+++ src/firebase.js
@@ -1,32 +1,25 @@
 import { initializeApp } from "firebase/app";
 
 
-/* const {
+const {
   REACT_APP_API_KEY,
   REACT_APP_AUTH_DOMAIN,
   REACT_APP_PROJECT_ID,
   REACT_APP_STORAGE_BUCKET,
   REACT_APP_MASSAGING_SENDER_ID,
   REACT_APP_APP_ID
-} = process.env */
+} = process.env
 
 
 
 const firebaseConfig = {
-  /* apiKey: REACT_APP_API_KEY,
+  apiKey: REACT_APP_API_KEY,
   authDomain: REACT_APP_AUTH_DOMAIN,
   projectId: REACT_APP_PROJECT_ID,
   storageBucket: REACT_APP_STORAGE_BUCKET,
   messagingSenderId: REACT_APP_MASSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID */
+  appId: REACT_APP_APP_ID
 
-
-  apiKey: "AIzaSyCiwNv2vbCsM1rmA9qPhAKvMR7rqIM5dHE",
-  authDomain: "ecommerce-react-sport.firebaseapp.com",
-  projectId: "ecommerce-react-sport",
-  storageBucket: "ecommerce-react-sport.appspot.com",
-  messagingSenderId: "197343704183",
-  appId: "1:197343704183:web:c76b72f2a1aed70d9dc272"
 };
 
 
